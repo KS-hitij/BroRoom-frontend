f98d08f2951f4df43aac9ccfd2ca8e44327d63d8
KS-hitij <149416585+KS-hitij@users.noreply.github.com>
2025-07-11 03:12:06 +0530

    Made layout more responsive

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { useSelector,useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import type { AppDispatch, RootState } from "../store"
 import { useEffect, useState, useRef } from "react";
 import { getSocket, setSocket } from "../utils/socketInstance";
@@ -31,7 +31,7 @@ export default function Room() {
     useEffect(() => {
         const ws: WebSocket = new WebSocket("wss://broroom-backend.onrender.com/");
         setSocket(ws);
-        let id:ReturnType<typeof setInterval>;
+        let id: ReturnType<typeof setInterval>;
         ws.onopen = () => {
             ws.send(JSON.stringify({
                 type: "join",
@@ -90,10 +90,10 @@ export default function Room() {
         setMessage("");
     }
 
-    const leaveRoom = ()=>{
+    const leaveRoom = () => {
         const ws = getSocket();
         ws.send(JSON.stringify({
-            type: "chat",
+            type: "leave",
             payload: {
                 name: user.name,
                 avatar: user.avatar,
@@ -103,11 +103,14 @@ export default function Room() {
         dispatch(clearUser());
         navigate("/");
     }
-    if(!getSocket()){
-        return <h1>Loading...</h1>
+    if (!getSocket()) {
+        return
+        <div className="h-[100%] w-[100%] flex justify-center items-center">
+            <span className="loading loading-spinner loading-xl"></span>
+        </div>
     }
     return (
-        <div className="h-[100%] w-[100%] flex flex-col lg:flex-row bg-base-300 relative">
+        <div className="h-[100%] w-[100%] flex  flex-col lg:flex-row bg-base-300 relative">
             <div className="h-[100%] hidden lg:flex flex-col pt-6 lg:w-[25%] ">
                 <div className="w-full mb-15">
                     <h1 className="text-center mb-5">Room Id: {user.roomId}</h1>
@@ -119,7 +122,7 @@ export default function Room() {
                             <div>
                                 <div className="avatar">
                                     <div className="w-16 rounded-full">
-                                        <img src={p.payload.avatar?p.payload.avatar:"/assets/default.jpg"} alt="" />
+                                        <img src={p.payload.avatar ? p.payload.avatar : "/assets/default.jpg"} alt="" />
                                     </div>
                                 </div>
                                 <h1 key={idx}>{p.payload.name}</h1>
@@ -131,12 +134,36 @@ export default function Room() {
                     <button type="button" onClick={leaveRoom} className="btn btn-error text-white font-bold btn-lg">Leave</button>
                 </div>
             </div>
-            <div className="navbar flex justify-between sticky top-0 lg:hidden h-[10%] bg-base-300">
-                    <h1 className="text-center text-xl font-semibold">Room Id: {user.roomId}</h1>
-                    <button type="button" onClick={leaveRoom} className="btn btn-error text-white font-bold btn-sm">Leave</button>
+            <div className="navbar flex px-5 justify-between z-20 sticky top-0 lg:hidden h-[10%] bg-base-300">
+                <h1 className="text-center text-xl font-semibold">Room Id: {user.roomId}</h1>
+                <div className="dropdown">
+                    <div title="aufha" tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm  dropdown-content gap-y-1 flex flex-col items-center right-0 bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li className="w-full"><button type="button" onClick={leaveRoom} className="btn btn-error mb-5 text-white font-bold btn-md">Leave</button></li>
+                        <li><h1 className="text-xl font-semibold">Users</h1></li>
+                        {participants.map((p, idx) => {
+                        return (
+                            <li className="w-full">
+                            <div className="flex justify-start w-full">
+                                <div className="avatar">
+                                    <div className="w-8 rounded-full">
+                                        <img src={p.payload.avatar ? p.payload.avatar : "/assets/default.jpg"} alt="" />
+                                    </div>
+                                </div>
+                                <h1 key={idx}>{p.payload.name}</h1>
+                            </div>
+                            </li>
+                        )
+                        })}
+                    </ul>
+                </div>
             </div>
             <div className="w-[100%] lg:w-[75%] h-[90%] lg:h-[100%] bg-base-100 py-1 px-0.5 flex flex-col justify-end ">
-                <div className="flex flex-col gap-y-3 h-[94%] text-xl overflow-y-scroll">
+                <div className="flex px-3 flex-col gap-y-3 h-[94%] text-xl overflow-y-scroll">
                     {
                         texts.map((t, idx) => (
                             <div className="chat chat-start" key={idx}>
@@ -150,9 +177,10 @@ export default function Room() {
                     }
                     <div ref={bottomRef} />
                 </div>
-                <div className="h-[10%] lg:h-[6%] w-full flex sticky bottom-0 lg:justify-around">
-                    <input type="text" onKeyDown={(e) => { if (e.key === "Enter") { sendChat() } }} onChange={(e) => setMessage(e.target.value)} value={message} className="input lg:w-[92%] active:outline-0 text-xl" placeholder="Message here" />
-                    <button type="button" onClick={sendChat} className="btn btn-primary lg:w-[7%]">Send</button>
+                <div className="h-[10%] lg:h-[6%] bottom-0 w-full flex sticky lg:justify-around">
+                    <input type="text" onKeyDown={(e) => { if (e.key === "Enter") { sendChat() } }} onChange={(e) => setMessage(e.target.value)} value={message}
+                        className="input w-[85%] sm:w-[92%] active:outline-0 text-xl" placeholder="Message here" />
+                    <button type="button" onClick={sendChat} className="btn w-[15%] btn-primary lg:w-[7%]">Send</button>
                 </div>
             </div>
         </div>
